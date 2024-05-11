ffd7da175f8aa1a6fb7dd503a3650fb65d3d93b3
Devesh Darshan Singh <sdevesh059@gmail.com>
2024-05-12 01:00:23 +0530

    Basics

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -2,5 +2,24 @@
 pragma solidity ^0.8.9;
 
 contract Dappazon {
-    uint a=10;
+
+  address public owner;
+
+  constructor(){
+
+    owner=msg.sender;
+  }
+
+  function list(uint256 _id,
+  string memory _name,
+  string memory _category,
+  string memory _image,
+  uint256 _cost,
+  uint256 _rating,
+  uint256 _stock) 
+  public{
+
+  }
+
+    
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -6,4 +6,24 @@ const tokens = (n) => {
 
 describe("Dappazon", () => {
 
+  let dappazon,deployer,buyer;
+
+  beforeEach(async()=>{
+
+    const signers=await ethers.getSigners();
+    deployer=signers[0];
+    buyer=signers[1];
+   
+    const Dappazon=await ethers.getContractFactory("Dappazon");
+    dappazon=await Dappazon.deploy();
+  })
+
+  describe("Deployement",()=>{
+
+    it("Sets the owner",async()=>{
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+
+  })
+
 })
